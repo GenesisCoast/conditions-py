7b2e6195df9bd7bd688d92480e115ca142d9791c
Harry Sanderson <h.sanderson@avanade.com>
2019-09-20 17:45:30 +0100

    Modified the object conditions to allow for multiple types to be specified, added if statement to skip type check on Null value

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='conditions_py',
-    version='0.1.5',
+    version='0.1.6',
     packages=[
         'conditions_py',
         'conditions_py.errors',
